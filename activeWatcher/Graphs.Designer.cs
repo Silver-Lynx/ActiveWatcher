d052d925d70c0326fa94e7268a6e119dad6066f0
Zaigane <Zaigane@gmail.com>
2020-07-13 11:08:55 -0400

    Changing data view to winforms chart

---
+++ activeWatcher/Graphs.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ActiveWatcher
+{
+    partial class Graphs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea1.AxisX.LabelStyle.Format = "h tt";
+            chartArea1.AxisX.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.MajorGrid.Interval = 0D;
+            chartArea1.AxisX.MajorGrid.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.MajorTickMark.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.MajorTickMark.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.MinorGrid.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.MinorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.MinorTickMark.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.MinorTickMark.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            chartArea1.AxisY.Interval = 15D;
+            chartArea1.AxisY.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.Maximum = 60D;
+            chartArea1.AxisY.MaximumAutoSize = 100F;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.Title = "Time Active (Minutes)";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.White;
+            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisY2.MaximumAutoSize = 80F;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.CursorX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Hours;
+            chartArea1.Name = "ChartArea";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            legend1.ForeColor = System.Drawing.Color.White;
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
+            series1.Legend = "Legend";
+            series1.Name = "Series1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(800, 450);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "Times";
+            this.chart1.TextAntiAliasingQuality = System.Windows.Forms.DataVisualization.Charting.TextAntiAliasingQuality.Normal;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dateTimePicker1.Location = new System.Drawing.Point(297, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Graphs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Graphs";
+            this.Text = "Graphs";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ activeWatcher/Graphs.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ActiveWatcher
+{
+    public partial class Graphs : Form
+    {
+        public Graphs()
+        {
+            InitializeComponent();
+
+            refreshData();
+        }
+
+        private void refreshData()
+        {
+            Dictionary<string, Series> seriesHold = new Dictionary<string, Series>();
+            List<Series> ignoreEmpty = new List<Series>();
+            //processes = new List<WProcess>();
+            //processTotal = 0;
+
+            //SeriesCollection timeSeries = new SeriesCollection();
+
+            ////Create a dictionary of chart values defined as datetime-int pairs, representing all lines to be drawn, linked to their index
+            //Dictionary<int, StackedColumnSeries> seriesHold = new Dictionary<int, StackedColumnSeries>();
+
+            ////Get time type
+            //string dateString = ((string)dateType.SelectedItem).ToUpper();
+            //double hourMult;
+            //switch (dateString)
+            //{
+            //    case "HOURS":
+            //        hourMult = 1.0;
+            //        break;
+            //    case "DAYS":
+            //        hourMult = 24.0;
+            //        break;
+            //    default:
+            //        hourMult = 0.0;
+            //        break;
+            //}
+            ////Get time value
+            //int dateVal = (int)dateValue.Value;
+            DateTime dateval = dateTimePicker1.Value;
+
+            ////Reset Axes
+            //cartesianChart1.Series.Configuration = Mappers.Xy<DateTimePoint>()
+            //    .X(value => (double)value.DateTime.Ticks / TimeSpan.FromMinutes(hourMult == 24.0 ? 60 : 1).Ticks)
+            //    .Y(value => value.Value);
+
+            //cartesianChart1.AxisX.Clear();
+
+            //cartesianChart1.AxisX.Add(new Axis
+            //{
+            //    MinValue = (double)DateTime.Now.AddHours(-1 * dateVal * hourMult).Ticks / TimeSpan.FromMinutes(hourMult == 24.0 ? 60 : 1).Ticks,
+            //    MaxValue = (double)DateTime.Now.Ticks / TimeSpan.FromMinutes(hourMult == 24.0 ? 60 : 1).Ticks,
+            //    Separator = new Separator
+            //    {
+            //        Step = hourMult == 24.0 ? 12 : 30,
+            //        IsEnabled = true
+            //    },
+
+            //    LabelFormatter = value => new DateTime((long)(value * TimeSpan.FromMinutes(hourMult == 24.0 ? 60 : 1).Ticks)).ToString("t")
+            //});
+
+            //cartesianChart1.AxisY[0].MaxValue = hourMult == 24.0 ? 3600 : 60;
+
+            //Open DB connection
+            SQLiteConnection database = new SQLiteConnection(Watcher.DBCONNECTION);
+            database.Open();
+
+            //Do query for times
+            using (SQLiteCommand comm = new SQLiteCommand())
+            {
+                //Build command
+                comm.CommandType = System.Data.CommandType.Text;
+                comm.CommandText = string.Format(@"
+                    WITH RECURSIVE times(hour,counter) AS (
+                        SELECT DATETIME('{0}'),0
+                        UNION ALL
+                        SELECT DATETIME(hour,'+1 hour'),counter+1
+                        FROM times
+                        WHERE counter < 23
+                    )
+                    SELECT p.common_name, COALESCE(a.counter/60,0), t.hour
+                    FROM times t
+                    join (SELECT process_ndx, common_name FROM process_ref UNION SELECT 0,'Idle') p
+                    ON 1=1
+                    LEFT JOIN ( SELECT COALESCE(process_ndx,0) AS 'process_ndx', COUNT(*) AS 'counter', DATETIME(ROUND(JULIANDAY(time_stamp) * 24.0 - 0.5) / 24.0, 'localtime') AS 'time_stamp'
+	                    FROM process_time_data
+	                    WHERE time_stamp BETWEEN DATETIME('{0}') AND DATETIME('{0}','+1 day')
+	                    GROUP BY process_ndx, ROUND(JULIANDAY(time_stamp) * 24.0 -0.5)) a
+                    ON a.process_ndx = p.process_ndx
+                    AND a.time_stamp = t.hour
+                    ORDER BY t.hour", dateval.ToString("yyyy-MM-dd"));
+                comm.Connection = database;
+
+                //Run command and get data
+                using (SQLiteDataReader data = comm.ExecuteReader())
+                {
+                    //Data exists
+                    if (data.HasRows)
+                    {
+                        //While data still exists
+                        while (data.Read())
+                        {
+                            string pname = data.GetString(0);
+                            int count = data.GetInt32(1);
+                            DateTime date = data.GetDateTime(2);
+
+                            if (!seriesHold.ContainsKey(pname))
+                            {
+                                Series hold = new Series();
+                                hold.ChartType = SeriesChartType.StackedArea;
+                                hold.Legend = "Legend";
+                                hold.ChartArea = "ChartArea";
+                                hold.Name = pname;
+                                hold.XValueType = ChartValueType.Time;
+                                seriesHold[pname] = hold;
+                                ignoreEmpty.Add(hold);
+                            }
+
+                            ////If index is not in dictionary
+                            //if (!seriesHold.ContainsKey(index))
+                            //{
+                            //    StackedColumnSeries hold = new StackedColumnSeries();
+                            //    hold.Values = new ChartValues<DateTimePoint>();
+                            //    //hold.LineSmoothness = 0;
+                            //    hold.ScalesXAt = 0;
+                            //    hold.MaxColumnWidth = 100;
+                            //    hold.ColumnPadding = 0;
+                            //    hold.PointGeometry = null;
+                            //    seriesHold.Add(index, hold);
+                            //}
+
+                            /*
+                            //Add a cutoff line if a process isn't used for a tick
+                            if (seriesHold[index].Values.Count > 0) {
+                                DateTime lastPoint = ((DateTimePoint)seriesHold[index].Values[seriesHold[index].Values.Count - 1]).DateTime;
+                                if (TimeSpan.FromTicks(date.Ticks - lastPoint.Ticks).TotalMinutes > 1) {
+                                    seriesHold[index].Values.Add(new DateTimePoint(lastPoint.AddMinutes(1), 0));
+                                    seriesHold[index].Values.Add(new DateTimePoint(date.AddMinutes(-1), 0));
+                                }
+                            }
+                            //Start at 0
+                            else
+                            {
+                                seriesHold[index].Values.Add(new DateTimePoint(date.AddMinutes(-1), 0));
+                            }
+                            */
+
+                            if (count > 0) ignoreEmpty.Remove(seriesHold[pname]);
+
+                            //Add value to series data
+                            //seriesHold[index].Values.Add(new DateTimePoint(date, data.GetInt32(1)));
+                            seriesHold[pname].Points.AddXY(date, count);
+                        }
+                    }
+                    //Close data reader
+                    data.Close();
+                }
+            }
+
+            ////Do query for processes
+            //using (SQLiteCommand comm = new SQLiteCommand())
+            //{
+            //    //Build command
+            //    comm.CommandType = System.Data.CommandType.Text;
+            //    comm.CommandText = string.Format(@"
+            //        SELECT COALESCE(p.process_ndx,0), p.common_name, p.icon, t.counter
+            //        FROM (SELECT process_ndx, COUNT(*) AS 'counter'
+            //             FROM process_time_data
+            //             WHERE time_stamp BETWEEN DATETIME('NOW', '-{0} {1}') AND DATETIME('NOW')
+            //             GROUP BY process_ndx) t
+            //        LEFT JOIN process_ref p
+            //        ON p.process_ndx = t.process_ndx
+            //        WHERE t.counter > 0
+            //        ORDER BY t.counter", dateVal, dateString);
+            //    comm.Connection = database;
+
+            //    //Run command and get data
+            //    using (SQLiteDataReader data = comm.ExecuteReader())
+            //    {
+            //        //Data exists
+            //        if (data.HasRows)
+            //        {
+            //            //While data still exists
+            //            while (data.Read())
+            //            {
+            //                WProcess hold;
+            //                if (data.GetInt32(0) == 0)
+            //                {
+            //                    hold = new WProcess(0, data.GetInt32(3).ToString(), "Idle Time", Properties.Resources.ZZZ);
+            //                }
+            //                else
+            //                {
+            //                    hold = new WProcess(data.GetInt32(0), data.GetInt32(3).ToString(), data.GetString(1), Utility.imageFromString(data.GetString(2)));
+            //                }
+
+            //                processes.Add(hold);
+            //                processTotal += int.Parse(hold.processName);
+            //            }
+            //        }
+            //        //Close data reader
+            //        data.Close();
+            //    }
+            //}
+
+            ////Clear label list
+            //foreach (Control c in labelPanel.Controls)
+            //{
+            //    c.Dispose();
+            //}
+            //labelPanel.Controls.Clear();
+            //labelPanel.Invalidate();
+
+            //processes.Sort((x, y) => int.Parse(x.processName) > int.Parse(y.processName) ? -1 : 1);
+
+            ////Repopulate Label list
+            //foreach (WProcess p in processes)
+            //{
+            //    IconLabel hold = new IconLabel();
+            //    hold.displayText = p.commonName;
+            //    hold.Image = p.icon;
+            //    hold.fillPercent = double.Parse(p.processName) / processTotal;
+            //    hold.resizeText();
+            //    hold.Location = new Point(0, processes.IndexOf(p) * 32);
+
+            //    labelPanel.Controls.Add(hold);
+
+            //    //Set matching series label for each process
+            //    if (seriesHold.ContainsKey(p.ID))
+            //    {
+            //        seriesHold[p.ID].Title = p.commonName;
+            //        seriesHold[p.ID].Values.Add(new DateTimePoint(((DateTimePoint)seriesHold[p.ID].Values[seriesHold[p.ID].Values.Count - 1]).DateTime.AddMinutes(1), 0));
+            //    }
+            //}
+
+            ////Build chart
+            //cartesianChart1.Series.Clear();
+
+            //cartesianChart1.Series.AddRange(seriesHold.Values);
+
+            //cartesianChart1.Refresh();
+            chart1.Series.Clear();
+            foreach (Series item in seriesHold.Values)
+            {
+                if(!ignoreEmpty.Contains(item))
+                    chart1.Series.Add(item);
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            refreshData();
+        }
+    }
+}
